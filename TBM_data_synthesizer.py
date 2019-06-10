ee80a0a0a707f730c5e915d20ba334f4c96f5daa
G. H. Erharter <32653899+geograz@users.noreply.github.com>
2019-06-10 14:50:52 +0200

    Update TBM_data_synthesizer.py

--- TBM_data_synthesizer.py
+++ TBM_data_synthesizer.py
@@ -14,7 +14,7 @@ tl = orig
 
 
 # parameters:
-noise = np.random.normal(10, 2, size=int(length/interval))
+noise = np.random.normal(11, 2, size=int(length/interval))
 penetration = np.sin(orig*0.01)
 penetration = penetration + np.cos(orig*0.25)
 penetration = penetration + np.cos(orig*7)
@@ -22,7 +22,7 @@ penetration = penetration + np.sin(orig*0.2)
 penetration = penetration + np.cos(orig*0.013)
 penetration = penetration + noise
 
-adv_force = np.random.normal(10000, 2500, size=int(length/interval))
+adv_force = np.random.normal(10000, 2000, size=int(length/interval))
 
 rpm = np.full(int(length/interval), 5)
 idx = np.where(adv_force > 15000)[0]
@@ -30,9 +30,9 @@ rpm[idx] = 4
 idx = np.where(adv_force < 5000)[0]
 rpm[idx] = 6
 
-speed = penetration * rpm
+speed = (penetration * rpm) / 1000
 
-noise = np.random.normal(2, 0.7, size=int(length/interval))
+noise = np.random.normal(2, 0.2, size=int(length/interval))
 torque = np.cos(orig)
 torque = torque + np.sin(orig*4)
 torque = torque + np.cos(orig*7)
@@ -40,6 +40,7 @@ torque = torque + np.sin(orig*0.2)
 torque = torque + np.sin(orig*0.05)
 torque = torque + noise
 torque = np.where(torque <= 0, np.median(torque), torque)
+torque = np.where(torque > 4.5, np.random.normal(2, 0.2), torque)
 
 
 # save data as csv file
@@ -47,7 +48,7 @@ df = pd.DataFrame({'tunnellength [m]': tl,
                    'penetration [mm/rot]': penetration,
                    'advance_force [kN]': adv_force,
                    'rev_per_min [rot/min]': rpm,
-                   'advance_speed [mm/min]': speed,
+                   'advance_speed [m/min]': speed,
                    'torque [MNm]': torque})
 
 df.to_csv('synth_TBM_data.csv', index=False)
